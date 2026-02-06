7c3f2b9aafbb6f3ecc7914e3ad5545c808be4732
Prasad Yemekar <emekarprasad@gmail.com>
2026-02-06 09:32:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+//your JS code here. If required.
+let codes = document.querySelectorAll(".code");
+
+// auto-focus first box
+codes[0].focus();
+
+codes.forEach((input, index) => {
+input.addEventListener("input", (e) => {
+const value = e.target.value;
+if(value.length === 1 && index < codes.length - 1) {
+codes[index+1].focus();
+}
+})
+
+
+input.addEventListener("keydown", (e) => {
+	if(e.key === "Backspace") {
+		if(input.value === "" && index > 0) {
+			codes[index-1].focus();
+		} else {
+			input.value = "";
+		}
+	}
+})
+
+
+
+})
